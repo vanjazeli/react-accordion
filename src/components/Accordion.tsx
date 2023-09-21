b468fefc4311fc2ea34f6c4d3b292d708af0d6db
vanjazeli <vanjazeli@gmail.com>
2023-09-21 21:16:56 +0200

    feat: Add no animation functionality

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,16 +1,40 @@
 import { AccordionItem } from '../types/AccordionItem';
 
+import { useRef } from 'react';
+
 type AccordionProps = {
 	items?: AccordionItem[];
 };
 
 const Accordion = ({ items }: AccordionProps) => {
+	const itemRefs = useRef<(HTMLLIElement | null)[]>([]);
+
+	const clickHandler = (itemIndex: number) => {
+		const targetItem = itemRefs.current[itemIndex];
+		if (targetItem?.classList.contains('accordion__item--active')) {
+			itemRefs.current.forEach((item) => {
+				item?.classList.remove('accordion__item--active');
+			});
+		} else {
+			itemRefs.current.forEach((item) => {
+				item?.classList.remove('accordion__item--active');
+			});
+			targetItem?.classList.add('accordion__item--active');
+		}
+	};
+
 	return (
 		<ul className="accordion">
 			{items?.map((item, itemIndex) => (
-				<li className="accordion__item" key={itemIndex}>
+				<li className="accordion__item" key={itemIndex} ref={(el) => (itemRefs.current[itemIndex] = el)}>
 					<span className="accordion__number">{itemIndex + 1 < 10 ? `0${itemIndex + 1}` : itemIndex + 1}</span>
-					<button className="accordion__button" type="button">
+					<button
+						className="accordion__button"
+						type="button"
+						onClick={() => {
+							clickHandler(itemIndex);
+						}}
+					>
 						<span className="accordion__title">{item.title}</span>
 						<span className="accordion__icon"></span>
 					</button>
