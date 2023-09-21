10a889cee756212018ae603d8c8d0214574f69fe
vanjazeli <vanjazeli@gmail.com>
2023-09-21 09:56:13 +0200

    feat: Add basic structure for Accordion component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
-function App() {
-	return <>Testing</>;
-}
+import Accordion from './components/Accordion';
+
+const App = () => {
+	const accordionItems = [
+		{
+			title: 'First item',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula sapien at justo lacinia, eget cursus purus aliquet. Nulla facilisi. Proin congue urna ut sem vestibulum, ac dignissim velit eleifend. Nullam venenatis, purus sit amet condimentum venenatis, quam turpis varius augue.',
+		},
+		{
+			title: 'Second item',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut ornare odio. Quisque in quam eu tortor efficitur efficitur eu vel tellus. Fusce quis nunc vitae metus lacinia elementum. Sed sem eros, bibendum nec cursus ac, varius id elit. Integer non tortor eget nulla posuere cursus in sit amet risus. In hac habitasse platea dictumst. Vivamus eget metus et nunc rutrum tristique.',
+		},
+		{
+			title: 'Third item',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed tincidunt viverra lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus at ex sed ex congue feugiat. Maecenas rhoncus justo id augue varius, sed luctus neque venenatis.',
+		},
+	];
+
+	return (
+		<div className="wrap">
+			<Accordion items={accordionItems} />
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/Accordion.tsx
@@ -0,0 +1,26 @@
+import { AccordionItem } from '../types/AccordionItem';
+
+type AccordionProps = {
+	items?: AccordionItem[];
+};
+
+const Accordion = ({ items }: AccordionProps) => {
+	return (
+		<ul className="accordion">
+			{items?.map((item, itemIndex) => (
+				<li className="accordion__item" key={itemIndex}>
+					<span className="accordion__number">{itemIndex + 1 < 10 ? `0${itemIndex + 1}` : itemIndex + 1}</span>
+					<button className="accordion__button" type="button">
+						<span className="accordion__title">{item.title}</span>
+						<span className="accordion__icon">+</span>
+					</button>
+					<div className="accordion__description-holder">
+						<p className="accordion__description">{item.description}</p>
+					</div>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+export default Accordion;
