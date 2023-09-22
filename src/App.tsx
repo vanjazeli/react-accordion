447d0a3b9ae5a2aa4bd28af4c90c90efaead4ffe
vanjazeli <vanjazeli@gmail.com>
2023-09-22 13:12:57 +0200

    feat: Add structure for multiple accordions

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,27 @@ const App = () => {
 		},
 		{
 			title: 'Are your rates competitive?',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed tincidunt viverra lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus at ex sed ex congue feugiat. Maecenas rhoncus justo id augue varius, sed luctus neque venenatis.',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed tincidunt viverra lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus at ex sed ex congue feugiat. Maecenas rhoncus justo id augue varius, sed luctus neque venenatis.',
 		},
 		{
 			title: 'Why do you have a monthly project cap?',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula sapien at justo lacinia, eget cursus purus aliquet. Nulla facilisi. Proin congue urna ut sem vestibulum, ac dignissim velit eleifend. Nullam venenatis, purus sit amet condimentum venenatis, quam turpis varius augue.',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula sapien at justo lacinia, eget cursus purus aliquet. Nulla facilisi. Proin congue urna ut sem vestibulum, ac dignissim velit eleifend. Nullam venenatis, purus sit amet condimentum venenatis, quam turpis varius augue.',
 		},
 	];
 
 	return (
-		<div className="wrap">
-			<Accordion items={accordionItems} />
-		</div>
+		<ul className="wrap">
+			<li className="wrap__item">
+				<h2 className="wrap__heading">Using Refs</h2>
+				<Accordion items={accordionItems} type={1} />
+			</li>
+			<li className="wrap__item">
+				<h2 className="wrap__heading">Using State</h2>
+				<Accordion items={accordionItems} type={2} />
+			</li>
+		</ul>
 	);
 };
 
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -4,49 +4,55 @@ import { useRef } from 'react';
 
 type AccordionProps = {
 	items?: AccordionItem[];
+	type: 1 | 2;
 };
 
-const Accordion = ({ items }: AccordionProps) => {
-	const itemRefs = useRef<(HTMLLIElement | null)[]>([]);
+const Accordion = ({ items, type }: AccordionProps) => {
+	if (type === 1) {
+		const itemRefs = useRef<(HTMLLIElement | null)[]>([]);
 
-	const clickHandler = (itemIndex: number) => {
-		const targetItem = itemRefs.current[itemIndex];
-		if (targetItem?.classList.contains('accordion__item--active')) {
-			itemRefs.current.forEach((item) => {
-				item?.classList.remove('accordion__item--active');
-			});
-		} else {
-			itemRefs.current.forEach((item) => {
-				item?.classList.remove('accordion__item--active');
-			});
-			targetItem?.classList.add('accordion__item--active');
-		}
-	};
+		const clickHandler = (itemIndex: number) => {
+			const targetItem = itemRefs.current[itemIndex];
+			if (targetItem?.classList.contains('accordion__item--active')) {
+				itemRefs.current.forEach((item) => {
+					item?.classList.remove('accordion__item--active');
+				});
+			} else {
+				itemRefs.current.forEach((item) => {
+					item?.classList.remove('accordion__item--active');
+				});
+				targetItem?.classList.add('accordion__item--active');
+			}
+		};
 
-	return (
-		<ul className="accordion">
-			{items?.map((item, itemIndex) => (
-				<li className="accordion__item" key={itemIndex} ref={(el) => (itemRefs.current[itemIndex] = el)}>
-					<span className="accordion__number">{itemIndex + 1 < 10 ? `0${itemIndex + 1}` : itemIndex + 1}</span>
-					<button
-						className="accordion__button"
-						type="button"
-						onClick={() => {
-							clickHandler(itemIndex);
-						}}
-					>
-						<span className="accordion__title">{item.title}</span>
-						<span className="accordion__icon"></span>
-					</button>
-					<div className="accordion__description-holder">
-						<div className="accordion__description-wrap">
-							<p className="accordion__description">{item.description}</p>
+		return (
+			<ul className="accordion">
+				{items?.map((item, itemIndex) => (
+					<li className="accordion__item" key={itemIndex} ref={(el) => (itemRefs.current[itemIndex] = el)}>
+						<span className="accordion__number">{itemIndex + 1 < 10 ? `0${itemIndex + 1}` : itemIndex + 1}</span>
+						<button
+							className="accordion__button"
+							type="button"
+							onClick={() => {
+								clickHandler(itemIndex);
+							}}
+						>
+							<span className="accordion__title">{item.title}</span>
+							<span className="accordion__icon"></span>
+						</button>
+						<div className="accordion__description-holder">
+							<div className="accordion__description-wrap">
+								<p className="accordion__description">{item.description}</p>
+							</div>
 						</div>
-					</div>
-				</li>
-			))}
-		</ul>
-	);
+					</li>
+				))}
+			</ul>
+		);
+	}
+	if (type === 2) {
+		return <>Exaple using state</>;
+	}
 };
 
 export default Accordion;
